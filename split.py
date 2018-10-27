6589c35859dea3a546a985828d8bcf495fc83ae8
Miguel Martin <miguel@miguel-martin.com>
2018-10-27 21:56:08 +1030

    split file

---
+++ split.py
@@ -0,0 +1,29 @@
+import random
+from pathlib import Path
+from shutil import copy
+
+path = Path('../data/cooking_clips')
+
+def get_videos(directory):
+    for video in directory.iterdir():
+        if video.suffix != '.avi':
+            continue
+        yield video
+
+vids = list(get_videos(path))
+
+TRAIN_SPLIT_RATIO=0.8
+
+train_dir = path / 'train'
+val_dir = path / 'val'
+
+from util.dir import ensure_exists
+ensure_exists(train_dir)
+ensure_exists(val_dir)
+
+for vid in vids:
+    r = random.uniform(0, 1)
+    if r <= TRAIN_SPLIT_RATIO:
+        copy(vid, str(train_dir))
+    else:
+        copy(vid, str(val_dir))

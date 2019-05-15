071d50f87e0be853c31db8dd26b64768c3f6fe6b
Miguel Martin <miguel@miguel-martin.com>
2019-05-15 19:59:36 +0930

    create_object to reflection conflict caused by txt2vid.util.pickle, hence the rename

--- txt2vid/util/reflection.py
+++ txt2vid/util/reflection.py
@@ -1,3 +1,5 @@
+import torch
+
 # https://stackoverflow.com/questions/452969/does-python-have-an-equivalent-to-java-class-forname
 def get_class(kls):
     parts = kls.split('.')
@@ -6,3 +8,40 @@ def get_class(kls):
     for comp in parts[1:]:
         m = getattr(m, comp)            
     return m
+
+def create_object(json_obj, **kwargs):
+    params = json_obj
+    clz = get_class(params['class'])
+
+    if 'args' in params:
+        args = params['args']
+        args.update(kwargs)
+        return clz(**args)
+
+    return clz()
+
+# creates an object from a json file
+# assumes json is of the form:
+#
+# { "class": <class-name>, "args": { "foo": 1, "bar": "abc" } }
+#
+# if args is not provided, will default construct. Otherwise,
+# args is passed as kwargs to ctor.
+#
+# use kwargs to provide additional arguments
+def create_object_file(json_file_path, **kwargs):
+    import json
+    with open(json_file_path) as in_f:
+        params = json.load(in_f)
+    assert('class' in params)
+
+    return create_object(params, **kwargs)
+
+if __name__ == '__main__':
+    obj = {'class': 'txt2vid.models.tgan.discrim.Discrim',\
+            'args': { \
+                'sequence_first': False,\
+                'in_channels': 2\
+             }}
+    obj = create_object(obj, mid_ch=90)
+    print(obj)

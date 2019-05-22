48d944878615cb02652879a48bcd7ad86d147aab
Miguel Martin <miguel@miguel-martin.com>
2019-05-22 15:26:34 +0930

    sent script

---
+++ scripts/run_sent.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+TXT_DATA=./msr/msr.pickle
+VOCAB=./msr/msr.vocab
+
+python3 txt2vid/train/txt.py --data $TXT_DATA --vocab $VOCAB --cuda --out /run/media/doubleu/Linux/honours/temp/sent_model_bi_sa --workers 3 --batch_size 64 --seed 1337 --teacher_force 0.5 --epoch 20 --separate_decoder

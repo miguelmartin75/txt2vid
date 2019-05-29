f18373cdeb91d34c1cfa35f0e407d997c3366ddf
Miguel Martin <miguel@miguel-martin.com>
2019-05-30 00:04:52 +0930

    resize frames in create cache

--- txt2vid/data/create_cache.py
+++ txt2vid/data/create_cache.py
@@ -5,11 +5,16 @@ import tqdm
 
 from txt2vid.data import read_video_file
 
+import cv2
+
 def get_videos(directory):
     for video in directory.iterdir():
         if video.suffix != '.avi':
             continue
         yield video
+
+def resize(frame, frame_size):
+    return cv2.resize(frame, (frame_size, frame_size))
 
 def main(args):
     video_dir = Path(args.dir)
@@ -24,7 +29,9 @@ def main(args):
             parent_path = Path(path_to_save).parent
             if not parent_path.exists():
                 parent_path.mkdir()
-            frame.save(path_to_save)
+            frame = resize(frame, args.frame_size)
+            cv2.imwrite(path_to_save, frame)
+            #frame.save(path_to_save)
 
             del frame
 
@@ -32,6 +39,7 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--dir', type=str, default=None, help='location of videos', required=True)
+    parser.add_argument('--frame_size', type=int, default=256, help='size of frame')
 
     args = parser.parse_args()
     main(args)

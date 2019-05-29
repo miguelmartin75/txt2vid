31c8f544910967c27475ca15ac8905f2cc256879
Miguel Martin <miguel@miguel-martin.com>
2019-05-30 01:34:52 +0930

    add dynamic number of down blocks to resnet3d

--- txt2vid/models/resnet3d.py
+++ txt2vid/models/resnet3d.py
@@ -5,7 +5,7 @@ from txt2vid.models.layers import ResidualBlock, DownBlock
 
 class Resnet3D(nn.Module):
 
-    def __init__(self, num_channels=3, mid_ch=64, which_conv=nn.Conv3d, which_pool=nn.AvgPool3d, cond_dim=0, num_down_blocks=4):
+    def __init__(self, num_channels=3, mid_ch=64, which_conv=nn.Conv3d, which_pool=nn.AvgPool3d, cond_dim=0, num_down_blocks=4, wide=False):
         super().__init__()
         self.activation = nn.ReLU(inplace=False)
 
@@ -13,25 +13,35 @@ class Resnet3D(nn.Module):
             which_conv(num_channels, mid_ch, 3, 1, padding=1, bias=False),
             self.activation,
             which_conv(mid_ch, mid_ch, 3, 1, padding=1, bias=False),
-            which_pool(1, 2)
+            which_pool((1, 2, 2), 2)
         )
-        skip_conn = nn.Sequential(which_pool(1, 2), which_conv(num_channels, mid_ch, 1))
+        skip_conn = nn.Sequential(which_pool((1, 2, 2), 2), which_conv(num_channels, mid_ch, 1))
         self.res_block = ResidualBlock(inner_module=res_path, identity_map=skip_conn)
 
-        self.down = []
+        #self.down0 = DownBlock(in_channels=64, out_channels=128, which_conv=which_conv, wide=False)
+        #self.down1 = DownBlock(in_channels=128, out_channels=256, which_conv=which_conv, wide=False)
+        #self.down2 = DownBlock(in_channels=256, out_channels=512, which_conv=which_conv, wide=False)
+        #self.down3 = DownBlock(in_channels=512, out_channels=1024, which_conv=which_conv, wide=False)
+        #self.fc = nn.Linear(1024, 1)
+
+        down = []
         in_ch = mid_ch
         out_ch = 128
         for i in range(num_down_blocks):
-            self.down.append(DownBlock(in_channels=in_ch, out_channels=out_ch,  which_conv=which_conv))
+            down.append(DownBlock(in_channels=in_ch, out_channels=out_ch,  which_conv=which_conv, wide=wide))
             in_ch = out_ch
             out_ch *= 2
 
-        self.down = nn.ModuleList(self.down)
+        self.down = nn.ModuleList(down)
         self.fc = nn.Linear(in_ch, 1)
     
     def forward(self, x=None, cond=None, xbar=None):
         x = self.res_block(x)
 
+        #x = self.down0(x)
+        #x = self.down1(x)
+        #x = self.down2(x)
+        #x = self.down3(x)
         for down in self.down:
             x = down(x)
 
@@ -51,8 +61,8 @@ if __name__ == '__main__':
     print(net)
     print(x.size())
 
-    #from torchsummary import summary
-    #summary(net, (3, 16, 128, 128))
+    from torchsummary import summary
+    summary(net, (3, 16, 128, 128))
 
     #net2d = Resnet3D(which_conv=nn.Conv2d, which_pool=nn.AvgPool2d).cuda()
     #print("Num params 2d  = %d" % count_params(net2d))

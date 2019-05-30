a17e7bfcce3a33ba7e76253d3628825faed4ba83
Miguel Martin <miguel@miguel-martin.com>
2019-05-30 22:56:39 +0930

    remove bias in resnet3d

--- txt2vid/models/resnet3d.py
+++ txt2vid/models/resnet3d.py
@@ -5,24 +5,18 @@ from txt2vid.models.layers import ResidualBlock, DownBlock
 
 class Resnet3D(nn.Module):
 
-    def __init__(self, num_channels=3, mid_ch=64, which_conv=nn.Conv3d, which_pool=nn.AvgPool3d, cond_dim=0, num_down_blocks=4, wide=False):
+    def __init__(self, num_channels=3, mid_ch=64, which_conv=nn.Conv3d, which_pool=nn.AvgPool3d, cond_dim=0, num_down_blocks=4, wide=True):
         super().__init__()
         self.activation = nn.ReLU(inplace=False)
 
         res_path = nn.Sequential(
-            which_conv(num_channels, mid_ch, 3, 1, padding=1, bias=False),
+            which_conv(num_channels, mid_ch, 3, 1, padding=1),
             self.activation,
-            which_conv(mid_ch, mid_ch, 3, 1, padding=1, bias=False),
+            which_conv(mid_ch, mid_ch, 3, 1, padding=1),
             which_pool((1, 2, 2), 2)
         )
         skip_conn = nn.Sequential(which_pool((1, 2, 2), 2), which_conv(num_channels, mid_ch, 1))
         self.res_block = ResidualBlock(inner_module=res_path, identity_map=skip_conn)
-
-        #self.down0 = DownBlock(in_channels=64, out_channels=128, which_conv=which_conv, wide=False)
-        #self.down1 = DownBlock(in_channels=128, out_channels=256, which_conv=which_conv, wide=False)
-        #self.down2 = DownBlock(in_channels=256, out_channels=512, which_conv=which_conv, wide=False)
-        #self.down3 = DownBlock(in_channels=512, out_channels=1024, which_conv=which_conv, wide=False)
-        #self.fc = nn.Linear(1024, 1)
 
         down = []
         in_ch = mid_ch
@@ -52,17 +46,28 @@ class Resnet3D(nn.Module):
 if __name__ == '__main__':
     from txt2vid.util.misc import count_params
 
-    x = torch.randn(1, 3, 16, 32, 32).cuda()
+    x = torch.randn(13, 3, 16, 128, 128).cuda()
     net = Resnet3D().cuda()
     print(net)
     print("Num params = %d" % count_params(net))
 
-    x = net(x)
-    print(net)
-    print(x.size())
+    z = net(x)
 
-    from torchsummary import summary
-    summary(net, (3, 16, 128, 128))
+    #import time
+    #while True:
+    #    z = net(x)
+    #    time.sleep(1000)
+
+    #print(net)
+    #print(x.size())
+
+    #from torchsummary import summary
+    #summary(net, (3, 16, 128, 128), batch_size=1)
+
+    print(torch.cuda.max_memory_allocated() / 10**9)
+    print(torch.cuda.max_memory_cached() / 10**9)
+
+    x = input()
 
     #net2d = Resnet3D(which_conv=nn.Conv2d, which_pool=nn.AvgPool2d).cuda()
     #print("Num params 2d  = %d" % count_params(net2d))
